dc50eeda8e4ddb9984e4e4371f7636fef71da3b2
Thomas Gelf <thomas@gelf.net>
2019-11-15 16:26:01 +0100

    BaseOperationalRequestForm: common base class

---
+++ library/Iet/Web/Form/BaseOperationalRequestForm.php
@@ -0,0 +1,189 @@
+<?php
+
+namespace Icinga\Module\Iet\Web\Form;
+
+use gipfl\Translation\TranslationHelper;
+use Icinga\Application\Icinga;
+use Icinga\Authentication\Auth;
+use Icinga\Exception\ConfigurationError;
+use Icinga\Module\Iet\Config;
+use Icinga\Web\Notification;
+use ipl\Html\Form;
+use ipl\Html\FormDecorator\DdDtDecorator;
+
+abstract class BaseOperationalRequestForm extends Form
+{
+    use TranslationHelper;
+
+    /** @@var \Icinga\Module\Iet\Api */
+    private $api;
+
+    private $cacheDir;
+
+    public function __construct()
+    {
+        $this->setDefaultElementDecorator(new DdDtDecorator());
+    }
+
+    abstract protected function addMessageDetails();
+
+    abstract protected function ack($ietKey);
+
+    protected function assemble()
+    {
+        try {
+            $ietInstances = Config::enumInstances();
+            $this->addElement('select', 'iet_instance', [
+                'label' => $this->translate('iET Instance'),
+                'multiOptions' => $ietInstances,
+                'required' => true,
+                'ignore'   => true,
+            ]);
+
+            if ($this->hasBeenSent()) {
+                $this->api = Config::getApi($this->getElement('iet_instance')->getValue());
+            } else {
+                $this->api = Config::getApi();
+            }
+        } catch (ConfigurationError $e) {
+            throw $e;
+            $this->addError($e);
+            $this->submitLabel = false;
+
+            return;
+        }
+
+        $myUsername = Auth::getInstance()->getUser()->getUserName();
+        // $reporters = $this->api->listReporters();
+        // $this->prefixEnumValueWithName($reporters);
+        if (null === ($allGroups = $this->getCached('all-groups'))) {
+            $allGroups = $this->api->listGroupsAssignable();
+        }
+        if (null === ($groups = $this->getCached('my-groups'))) {
+            $groups = $this->api->listRepGroups($myUsername);
+        }
+        $this->prefixEnumValueWithName($allGroups);
+        $this->prefixEnumValueWithName($groups);
+        if (null === ($sourceSystems = $this->getCached('source-systems'))) {
+            $sourceSystems = $this->api->listSourceSystems();
+        }
+
+        $this->addElement('select', 'sourcesystemid', [
+            'label' => $this->translate('Source System'),
+            'multiOptions' => $this->optionalEnum($sourceSystems),
+            'required' => true,
+        ]);
+        $this->addElement('select', 'repgrp', [
+            'label' => $this->translate('Group (Reporter)'),
+            'multiOptions' => $groups,
+            'required' => true,
+        ]);
+        $this->addElement('text', 'rep', [
+            'label' => $this->translate('Reporter'),
+            // 'multiOptions' => $this->optionalEnum($reporters),
+            'value' => $myUsername,
+            'required' => true,
+        ]);
+        $this->addElement('text', 'caller', [
+            'label' => $this->translate('Caller'),
+            // 'multiOptions' => $this->optionalEnum($reporters),
+            'value' => $myUsername,
+            'required' => true,
+        ]);
+        $this->addElement('select', 'fe', [
+            'label' => $this->translate('FE'),
+            'multiOptions' => $this->optionalEnum($allGroups),
+            'required' => true,
+        ]);
+
+        $this->addMessageDetails();
+
+        $this->addElement('submit', 'submit', [
+            'label' => $this->translate('Create')
+        ]);
+    }
+
+    protected function prefixEnumValueWithName(& $enum)
+    {
+        foreach ($enum as $name => $value) {
+            if ($name !== $value) {
+                $enum[$name] = "$name: $value";
+            }
+        }
+    }
+
+    public function setSuccessUrl($url)
+    {
+    }
+
+    public function onSuccess()
+    {
+        $key = $this->createOperationalRequest();
+        $message = "New Operational Request $key has been created";
+        Notification::success($message);
+    }
+
+    protected function optionalEnum($enum)
+    {
+        return [null => $this->translate('- please choose -')] + $enum;
+    }
+
+    private function createOperationalRequest()
+    {
+        $key = $this->api->createOR($this->getValues());
+        $this->ack($key);
+
+        return $key;
+    }
+
+    protected function makeEnum($data, $key, $name, $reject = null)
+    {
+        $enum = [];
+        foreach ($data as $entry) {
+            if (is_callable($reject) && $reject($entry)) {
+                continue;
+            }
+            $value = $this->getProperty($entry, $key);
+            $caption = $this->getProperty($entry, $name, $value);
+            $enum[$value] = $caption;
+        }
+
+        return $enum;
+    }
+
+    protected function getProperty($entry, $name, $default = null)
+    {
+        if (property_exists($entry, $name)) {
+            $value = $entry->$name;
+        }
+
+        if (empty($value)) {
+            return $default;
+        } else {
+            return $value;
+        }
+    }
+
+    protected function getCacheDir()
+    {
+        if ($this->cacheDir === null) {
+            // TODO: tmpdir?
+            $this->cacheDir = Icinga::app()
+                ->getModuleManager()
+                ->getModule('iet')
+                ->getBaseDir();
+        }
+
+        return $this->cacheDir;
+    }
+
+    protected function getCached($key)
+    {
+        $file =  $this->getCacheDir() . "/iet-$key.json";
+        if (file_exists($file)) {
+            return (array) \json_decode(\file_get_contents($file));
+        } else {
+            return null;
+        }
+    }
+}
--- library/Iet/Web/Form/CreateOperationalRequestForm.php
+++ library/Iet/Web/Form/CreateOperationalRequestForm.php
@@ -2,114 +2,20 @@
 
 namespace Icinga\Module\Iet\Web\Form;
 
-use gipfl\Translation\TranslationHelper;
 use Exception;
-use Icinga\Application\Icinga;
 use Icinga\Application\Logger;
-use Icinga\Authentication\Auth;
-use Icinga\Exception\ConfigurationError;
-use Icinga\Module\Iet\Config;
 use Icinga\Module\Iet\IcingaCommandPipe;
 use Icinga\Module\Monitoring\Object\MonitoredObject;
 use Icinga\Module\Monitoring\Object\Host;
 use Icinga\Module\Monitoring\Object\Service;
-use Icinga\Web\Notification;
-use ipl\Html\Form;
-use ipl\Html\FormDecorator\DdDtDecorator;
-use ipl\Html\Html;
 
-class CreateOperationalRequestForm extends Form
+class CreateOperationalRequestForm extends BaseOperationalRequestForm
 {
-    use TranslationHelper;
-
     /** @var  MonitoredObject */
     private $object;
 
-    /** @@var \Icinga\Module\Iet\Api */
-    private $api;
-
-    public function __construct()
+    protected function addMessageDetails()
     {
-        $this->setDefaultElementDecorator(new DdDtDecorator());
-    }
-
-    protected function getCached($key)
-    {
-        $file = Icinga::app()->getModuleManager()->getModule('iet')->getBaseDir() . '/iet-' . $key . '.json';
-        if (file_exists($file)) {
-            return (array) \json_decode(\file_get_contents($file));
-        } else {
-            return null;
-        }
-    }
-
-    protected function assemble()
-    {
-        try {
-            $ietInstances = Config::enumInstances();
-            $this->addElement('select', 'iet_instance', [
-                'label' => $this->translate('iET Instance'),
-                'multiOptions' => $ietInstances,
-                'required' => true,
-                'ignore'   => true,
-            ]);
-
-            if ($this->hasBeenSent()) {
-                $this->api = Config::getApi($this->getElement('iet_instance')->getValue());
-            } else {
-                $this->api = Config::getApi();
-            }
-        } catch (ConfigurationError $e) {
-            throw $e;
-            $this->addError($e);
-            $this->submitLabel = false;
-
-            return;
-        }
-
-        $myUsername = Auth::getInstance()->getUser()->getUserName();
-        // $reporters = $this->api->listReporters();
-        // $this->prefixEnumValueWithName($reporters);
-        if (null === ($allGroups = $this->getCached('all-groups'))) {
-            $allGroups = $this->api->listGroupsAssignable();
-        }
-        if (null === ($groups = $this->getCached('my-groups'))) {
-            $groups = $this->api->listRepGroups($myUsername);
-        }
-        $this->prefixEnumValueWithName($allGroups);
-        $this->prefixEnumValueWithName($groups);
-        if (null === ($sourceSystems = $this->getCached('source-systems'))) {
-            $sourceSystems = $this->api->listSourceSystems();
-        }
-
-        $this->addElement('select', 'sourcesystemid', [
-            'label' => $this->translate('Source System'),
-            'multiOptions' => $this->optionalEnum($sourceSystems),
-            'required' => true,
-        ]);
-        $this->addElement('select', 'repgrp', [
-            'label' => $this->translate('Group (Reporter)'),
-            'multiOptions' => $groups,
-            'required' => true,
-        ]);
-        $this->addElement('text', 'rep', [
-            'label' => $this->translate('Reporter'),
-            // 'multiOptions' => $this->optionalEnum($reporters),
-            'value' => $myUsername,
-            'required' => true,
-        ]);
-        $this->addElement('text', 'caller', [
-            'label' => $this->translate('Caller'),
-            // 'multiOptions' => $this->optionalEnum($reporters),
-            'value' => $myUsername,
-            'required' => true,
-        ]);
-        $this->addElement('select', 'fe', [
-            'label' => $this->translate('FE'),
-            'multiOptions' => $this->optionalEnum($allGroups),
-            'required' => true,
-        ]);
-
         $this->addElement('text', 'title1', [
             'label'       => $this->translate('Title'),
             'required'    => true,
@@ -128,28 +34,29 @@ class CreateOperationalRequestForm extends Form
                 'Message body of this issue'
             ),
         ));
-
-        $this->addElement('submit', 'submit', [
-            'label' => $this->translate('Create')
-        ]);
     }
 
-    protected function prefixEnumValueWithName(& $enum)
+    protected function ack($ietKey)
     {
-        foreach ($enum as $name => $value) {
-            if ($name !== $value) {
-                $enum[$name] = "$name: $value";
-            }
-        }
-    }
-
-    private function getObjectDefault($key)
-    {
-        $defaults = $this->getObjectDefaults();
-        if (array_key_exists($key, $defaults)) {
-            return $defaults[$key];
+        $object = $this->object;
+        $host = $object->host_name;
+        if ($object->getType() === 'service') {
+            $service = $object->service_description;
         } else {
-            return null;
+            $service = null;
+        }
+
+        $instance = $this->getValue('iet_instance');
+        $ackMessage = "Operational Request $instance:$ietKey has been created";
+
+        try {
+            $cmd = new IcingaCommandPipe();
+            if ($cmd->acknowledge("iET ($instance)", $ackMessage, $host, $service)) {
+                Logger::info("Problem has been acknowledged for $ietKey");
+            }
+        } catch (Exception $e) {
+            Logger::error($e->getMessage());
+            throw $e;
         }
     }
 
@@ -158,6 +65,16 @@ class CreateOperationalRequestForm extends Form
         $this->object = $object;
 
         return $this;
+    }
+
+    private function getObjectDefault($key)
+    {
+        $defaults = $this->getObjectDefaults();
+        if (\array_key_exists($key, $defaults)) {
+            return $defaults[$key];
+        } else {
+            return null;
+        }
     }
 
     private function getObjectDefaults()
@@ -192,80 +109,9 @@ class CreateOperationalRequestForm extends Form
     {
         $object = $this->object;
         if ($object->getType() === 'service') {
-            return strtoupper(Service::getStateText($object->service_state));
+            return \strtoupper(Service::getStateText($object->service_state));
         } else {
-            return strtoupper(Host::getStateText($object->host_state));
-        }
-    }
-
-    public function setSuccessUrl($url)
-    {
-    }
-
-    public function onSuccess()
-    {
-        $key = $this->createOperationalRequest();
-        $message = "New Operational Request $key has been created";
-        Notification::success($message);
-    }
-
-    protected function optionalEnum($enum)
-    {
-        return [null => $this->translate('- please choose -')] + $enum;
-    }
-
-    private function createOperationalRequest()
-    {
-        $object = $this->object;
-        $host = $object->host_name;
-        if ($object->getType() === 'service') {
-            $service = $object->service_description;
-        } else {
-            $service = null;
-        }
-
-        $key = $this->api->createOR($this->getValues());
-        $instance = $this->getValue('iet_instance');
-        $ackMessage = "Operational Request $instance:$key has been created";
-
-        try {
-            $cmd = new IcingaCommandPipe();
-            if ($cmd->acknowledge("iET ($instance)", $ackMessage, $host, $service)) {
-                Logger::info("Problem has been acknowledged for $key");
-            }
-        } catch (Exception $e) {
-            Logger::error($e->getMessage());
-            $this->notifyError($e->getMessage());
-        }
-
-        return $key;
-    }
-
-    protected function makeEnum($data, $key, $name, $reject = null)
-    {
-        $enum = [];
-        foreach ($data as $entry) {
-            if (is_callable($reject) && $reject($entry)) {
-                continue;
-            }
-            $value = $this->getProperty($entry, $key);
-            $caption = $this->getProperty($entry, $name, $value);
-            $enum[$value] = $caption;
-        }
-
-        return $enum;
-    }
-
-    protected function getProperty($entry, $name, $default = null)
-    {
-        if (property_exists($entry, $name)) {
-            $value = $entry->$name;
-        }
-
-        if (empty($value)) {
-            return $default;
-        } else {
-            return $value;
+            return \strtoupper(Host::getStateText($object->host_state));
         }
     }
 }
