7dba3cfcaf1849cd8fe43a18f9c5efe47aba1d64
Thomas Gelf <thomas@gelf.net>
2020-02-03 14:57:18 +0100

    OperationalRequestDetails: show more details

--- library/Iet/Web/Widget/OperationalRequestDetails.php
+++ library/Iet/Web/Widget/OperationalRequestDetails.php
@@ -5,7 +5,9 @@ namespace Icinga\Module\Iet\Web\Widget;
 use gipfl\IcingaWeb2\Widget\NameValueTable;
 use gipfl\Translation\TranslationHelper;
 use Icinga\Module\Iet\OperationalRequest;
+use Icinga\Module\Iet\WorklogEntry;
 use ipl\Html\BaseHtmlElement;
+use ipl\Html\Html;
 
 class OperationalRequestDetails extends BaseHtmlElement
 {
@@ -26,8 +28,38 @@ class OperationalRequestDetails extends BaseHtmlElement
 
     protected function assemble()
     {
+        $or = $this->or;
         $this->add((new NameValueTable())->addNameValuePairs([
-            $this->translate('ID') => $this->or->id,
+            $this->translate('Title')  => $or->title,
+            $this->translate('ID')     => $or->id,
+            $this->translate('Caller') => $or->caller,
+            $this->translate('Reporter (Group)') => \sprintf(
+                '%s (%s)',
+                $or->rep,
+                $or->repgrp
+            ),
+            $this->translate('FE (Rep)') => \sprintf(
+                '%s (%s)',
+                $or->fe,
+                $or->ferep
+            ),
         ]));
+
+        $this->add(Html::tag('pre', $this->or->details));
+
+        if (empty($or->worklog)) {
+            return;
+        }
+
+        $this->add(Html::tag('h3', $this->translate('Worklog')));
+        /** @var WorklogEntry $entry */
+        foreach ($or as $entry) {
+            $this->add(Html::tag('div', [
+                'class' => 'iet-worklog-entry'
+            ], [
+                Html::tag('h4', $entry->topic),
+                Html::tag('p', $entry->entry)
+            ]));
+        }
     }
 }
