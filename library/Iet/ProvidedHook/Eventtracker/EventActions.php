e9291342f06546c205dd2ecccece7919c67d2713
Thomas Gelf <thomas@gelf.net>
2020-02-06 14:18:48 +0100

    EventActions: implement getIssuesActions

--- library/Iet/ProvidedHook/Eventtracker/EventActions.php
+++ library/Iet/ProvidedHook/Eventtracker/EventActions.php
@@ -2,11 +2,15 @@
 
 namespace Icinga\Module\Iet\ProvidedHook\Eventtracker;
 
+use gipfl\IcingaWeb2\Url;
 use gipfl\Translation\TranslationHelper;
 use gipfl\IcingaWeb2\Link;
 use Icinga\Authentication\Auth;
+use Icinga\Data\Filter\Filter;
 use Icinga\Module\Eventtracker\Hook\EventActionsHook;
 use Icinga\Module\Eventtracker\Issue;
+use Icinga\Module\Eventtracker\SetOfIssues;
+use ipl\Html\Html;
 
 class EventActions extends EventActionsHook
 {
@@ -35,6 +39,32 @@ class EventActions extends EventActionsHook
                 'class' => 'icon-forward',
                 'title' => $this->translate('Create Operational Request'),
             ]);
+        }
+
+        return $links;
+    }
+
+    /**
+     * @param SetOfIssues $issues
+     * @return array
+     */
+    public function getIssuesActions(SetOfIssues $issues)
+    {
+        $auth = Auth::getInstance();
+        $links = [];
+
+        if ($auth->hasPermission('iet/or/create')) {
+            $filter = Filter::matchAny();
+            foreach ($issues->getIssues() as $issue) {
+                $filter->addFilter(Filter::where('uuid', $issue->getHexUuid()));
+            }
+            $link = Url::fromPath('iet/or/issue');
+            $url = $link->getAbsoluteUrl() . '?' . $filter->toQueryString();
+            $links[] = Html::tag('a', [
+                'href'  => $url,
+                'class' => 'icon-forward',
+                'title' => $this->translate('Create Operational Request'),
+            ], $this->translate('Create OR'));
         }
 
         return $links;
